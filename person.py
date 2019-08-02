2e82f124c444eb0be5c87b898a9712bf095cba61
abritodejesus <48891098+abritodejesus@users.noreply.github.com>
2019-08-02 18:12:41 -0300

    Delete person.py

--- person.py
+++
@@ -1,7 +0,0 @@
-class Person(object):
-    def __init__(self,person_id,image_uri):
-        self.person_id = person_id
-        self.image_uri = image_uri
-        
-    def __str__(self):
-        return self.person_id+" - "+self.image_uri
